08cbc64b2188204bb468b8c07b824251d7f2fd9c
Sam Wright <samwright@Sams-MacBook-Pro-3.local>
2023-02-27 01:13:41 -0600

    Added routes and api functionality

---
+++ Develop/server.js
@@ -0,0 +1,22 @@
+// Require and express are needed for app functionality
+const express = require("express");
+const app = express();
+
+// Import custom middleware
+app.use(clog);
+
+// Port used
+const PORT = process.env.port || 3001;
+
+// Middleware for parsing JSON and generate route
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Generated routes
+require("./routes/notesRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+// App is now listening 
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`));
